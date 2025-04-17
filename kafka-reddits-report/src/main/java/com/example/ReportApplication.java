af2509eb74b4f49c2205059a4c3feac693453d78
Oleksandr <gavrylenko.aleksandr@gmail.com>
2025-04-17 16:19:52 +0200

    Added kafka-reddits-report.

---
+++ kafka-reddits-report/src/main/java/com/example/ReportApplication.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * @author Oleksandr Havrylenko
+ **/
+public class ReportApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
